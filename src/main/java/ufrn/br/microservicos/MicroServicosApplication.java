9599d8dbde5deeb0d30c1d0450406e8898ff446e
Giovanna Oliveira <giovannaolvr2003@gmail.com>
2023-09-26 22:07:26 -0300

    microservicos - arquitetura de software

---
+++ src/main/java/ufrn/br/microservicos/MicroServicosApplication.java
@@ -0,0 +1,13 @@
+package ufrn.br.microservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicosApplication.class, args);
+	}
+
+}
